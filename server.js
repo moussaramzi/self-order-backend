901e0c7dfd5ae247218eb83060d80b4b06213758
Moussa Ramzi <r0881426@student.thomasmore.be>
2025-04-27 20:40:58 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('McDonalds API is running...');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
